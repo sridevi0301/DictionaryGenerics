e49623b6870165e49c64e4f444430dbe00df67a3
prem chand <premchandgurram19@gmail.com>
2026-03-04 10:38:59 +0530

    Add project files.

---
+++ DictionaryGenerics/Program.cs
@@ -0,0 +1,43 @@
+﻿class DictionaryManager<TKey, TValue>
+{
+    private readonly Dictionary<TKey,TValue> _dictionary;
+    public DictionaryManager()
+    {
+        _dictionary=new Dictionary<TKey,TValue>();
+    }
+    public void AddItem(TKey key, TValue value)
+    {
+        _dictionary.Add(key, value);
+    }
+    public TValue GetItem(TKey key)
+    {
+        return _dictionary[key];
+    }
+    public void RemoveItem(TKey key)
+    {
+        _dictionary.Remove(key);
+    }
+    public void DisplayAllItems()
+    {
+        foreach(var kvp in _dictionary)
+        {
+            Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+        }
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DictionaryManager<string, int> products=new DictionaryManager<string, int>();
+        products.AddItem("Laptop", 120);
+        products.AddItem("Smartphone", 80);
+        products.AddItem("Tablet", 50);
+        Console.WriteLine("All Products:");
+        products.DisplayAllItems();
+        products.RemoveItem("Smartphone");
+        Console.WriteLine("\nAfter Removing Smartphone:");
+        products.DisplayAllItems();
+
+    }
+}
